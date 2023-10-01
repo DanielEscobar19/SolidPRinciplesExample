17a80ddb68e1ded8750655a8df15980f831389a8
DanielEscobar19 <danielonchio@gmail.com>
2023-09-30 23:26:50 -0600

    SRP principle

---
+++ SolidPrinciplesExample/UserServiceExample/Services.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciplesExample.UserServiceExample
+{
+    public class UserService
+    {
+        private EmailService emailService;
+        public UserService(EmailService aEmailService) { 
+            this.emailService = emailService;
+        }
+        public void Register(string email)
+        {
+            if (emailService.ValidateEmail(email) == false)
+            {
+                throw new ValidationException("Email is not an email");
+            }
+
+            emailService.SendEmail("Hello I just registered my account!");
+        }
+    }
+
+    public class EmailService {
+        public bool ValidateEmail(string email)
+        {
+            return email.Contains("@");
+        }
+
+        public void SendEmail(string message)
+        {
+            Console.WriteLine($"Sending messahe: {message}");
+        }
+    }
+}
